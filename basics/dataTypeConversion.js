c37aaf0c491658edc7f45ee7f979b43f1bb83620
amankrgupta97 <70031251+amankrgupta97@users.noreply.github.com>
2024-03-09 10:12:48 +0000

    type conversion and operations

--- basics/dataTypeConversion.js
+++ basics/dataTypeConversion.js
@@ -1,26 +1,63 @@
-let score1="33"
-console.log(Number(score1)) //33 => 33
+// let score1="33"
+// console.log(Number(score1)) //33 => 33
 
-let score2="33abc"
-console.log(Number(score2)) //33abc => NaN
+// let score2="33abc"
+// console.log(Number(score2)) //33abc => NaN
 
-let score3=null
-console.log(Number(score3)) //null => 0
+// let score3=null
+// console.log(Number(score3)) //null => 0
 
-let score4=true
-console.log(Number(score4)) //true => 1
+// let score4=true
+// console.log(Number(score4)) //true => 1
 
-let name1=""
-console.log(Boolean(name1)) //"" => false
+// let name1=""
+// console.log(Boolean(name1)) //"" => false
 
-let name2="aman"
-console.log(Boolean(name2)) //"aman" => true
+// let name2="aman"
+// console.log(Boolean(name2)) //"aman" => true
 
-let num1=NaN
-console.log(Boolean(NaN)) // NaN => false
+// let num1=NaN
+// console.log(Boolean(NaN)) // NaN => false
 
-let obj1=null
-console.log(Boolean(obj1)) // null => false
+// let obj1=null
+// console.log(Boolean(obj1)) // null => false
+
+//*************************operations*************************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);addition
+// console.log(2-2);subtraction
+// console.log(2*2);multiplication
+// console.log(2**3); 2 to power 3
+// console.log(2/3);division
+// console.log(2%3); remainder
+
+let str1 = "hello"
+let str2 = " aman"
+
+let str3 = str1 + str2
+// console.log(str3); //hello aman; 
+
+// console.log("1" + 2);  //12 
+// console.log(1 + "2");  //12 
+// console.log("1" + 2 + 2); //122 concatination
+// console.log(1 + 2 + "2");  //32 addition then concatination
+// console.log(+true);// 1 + prefix convert to type number
+// console.log(+"");// 0 +prefix convert to type number
+
+// let x=3
+// const y=x++
+// console.log("x:",x,"y:",y) // x=4 y=3 postfix use first increment later
+
+// let x=3
+// const y=++x
+
+// console.log("x:",x,"y:",y)  //x=4 y=4 prefix increment first use later 
+
+
 
 
 
