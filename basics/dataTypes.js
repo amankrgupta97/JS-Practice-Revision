f5cff1b8bdf3fbfad82c3cc62dc4053334441b3c
amankrgupta97 <70031251+amankrgupta97@users.noreply.github.com>
2024-03-09 07:37:43 +0000

    datatypes in js

---
+++ basics/dataTypes.js
@@ -0,0 +1,19 @@
+let name="aman" //string
+let age=27 // number
+let isHuman=true //boolean
+let alpha;// undefined
+
+//types
+
+//number 2 to power 53
+//bigint
+//string => ""
+//boolean =>true/false
+//undefined => no value assigned
+// null => to represent empty value typeOf null is object
+//symbol => unique
+
+//objects
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) //object
