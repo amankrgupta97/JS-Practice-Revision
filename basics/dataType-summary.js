d219a914f166bd904c5d54e7c22ecee60970cea2
amankrgupta97 <70031251+amankrgupta97@users.noreply.github.com>
2024-03-09 11:15:32 +0000

    summary stack vs heap added

--- basics/dataType-summary.js
+++ basics/dataType-summary.js
@@ -1,6 +1,6 @@
 //  Primitive
 
-//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+//  7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
 const score = 100
 const scoreValue = 100.3
@@ -39,3 +39,25 @@ console.log(typeof bigNumber); //bigint
 console.log(typeof heros); //object
 console.log(typeof myObj); //object
 console.log(typeof myFunction); //function
+
+//Stack (primitive) 
+
+let user1="amankrgupta@gmail.com"
+let user2=user1;
+console.log(user2) //amankrgupta97@gmail.com
+user2="akgupta@gmail.com"
+console.log(user1); //amankrgupta97@gmail.com
+console.log(user2); //akgupta97@gmail.com
+
+// heap memory 
+
+const obj1={
+    email:"aman@gmail.com",
+    age:27,
+}
+
+const obj2=obj1;
+console.log(obj2.email); //aman@gmail.com
+obj2.email="amangupta@gmail.com"
+console.log(obj1.email); //amangupta@gmail.com
+console.log(obj2.email); //amangupta@gmail.com
