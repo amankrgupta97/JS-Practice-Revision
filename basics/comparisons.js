8e78ce28bd48be516b0bc02e735cd80dd6f3a1e3
amankrgupta97 <70031251+amankrgupta97@users.noreply.github.com>
2024-03-09 10:34:19 +0000

    comparsions <,>,>=,==,=== in Js

---
+++ basics/comparisons.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1); // true >,<,>= converts datatype into numbers while ==,not
+// console.log("02" > 1);//true
+
+// console.log(null > 0); //false => null is converted to number which is 0
+// console.log(null == 0); // false => null not converted
+// console.log(null >= 0); // true =>null is converted to number which is 0
+
+// console.log(undefined == 0); //false
+// console.log(undefined > 0); //false
+// console.log(undefined < 0); //true
+
+// === 
+
+// console.log("2" === 2); //false === check type and value == check only value
